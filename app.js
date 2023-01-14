dcc38d9b0ef3d7a8b4c9f691eb0093225d0ebe91
selim-haspolat <selimhaspolat@gmail.com>
2023-01-14 15:15:47 +0300

    bug fixed

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ checkBtn.addEventListener("click", () => {
   answer = input.value;
   !isNaN(answer) || alert("Enter a Number");
   answer == random ? findNum() : closestNums();
-  if(counter >= 3){
+  if(counter > 3){
     remain.innerText = 'Youl Lose'
     return result.innerText = `Answer is ${random}`
   }
@@ -33,7 +33,7 @@ checkBtn.addEventListener("click", () => {
 });
 
 const findNum = () => {
-  if(counter >= 3){
+  if(counter > 3){
     return null
   }
   alert("You Find")
@@ -41,7 +41,7 @@ const findNum = () => {
 }
 
 const closestNums = () => {
-  if(counter >= 3){
+  if(counter > 3){
     remain.innerText = 'Youl Lose'
     return result.innerText = `Answer is ${random}`
   }
